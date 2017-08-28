10ebb3e6db52baeca25b84b5b946533fbb92df28
Brayden Neale <Brayden.Neale@dynatrace.com>
2017-08-28 11:49:55 +1000

    Pull down vim config into vagrant box

--- provision.sh
+++ provision.sh
@@ -8,3 +8,7 @@ yum install -y curl git vim
 # Ansible
 yum install -y epel-release
 yum install -y ansible
+
+# TODO: if curl installed check
+# Setup vim config
+curl -o /home/vagrant/.vimrc https://raw.githubusercontent.com/BraydenNeale/vimrc/master/.vimrc
