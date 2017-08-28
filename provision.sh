7557a584bfbc090474edbc781eeb5b8f94755cf8
Brayden Neale <Brayden.Neale@dynatrace.com>
2017-08-28 11:57:09 +1000

    Install docker

--- provision.sh
+++ provision.sh
@@ -12,3 +12,12 @@ yum install -y ansible
 # TODO: if curl installed check
 # Setup vim config
 curl -o /home/vagrant/.vimrc https://raw.githubusercontent.com/BraydenNeale/vimrc/master/.vimrc
+
+# Docker - according to offical docs
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager \
+        --add-repo \
+            https://download.docker.com/linux/centos/docker-ce.repo
+yum makecache fast
+yum install -y docker-ce
+systemctl start docker
